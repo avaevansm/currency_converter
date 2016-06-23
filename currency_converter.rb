22acd64cbbb8848a789dbc916c9c2a25563ec15e
Ava Evans <avaevansm@gmail.com>
2016-06-23 15:13:51 -0400

    first commit

---
+++ currency_converter.rb
@@ -0,0 +1,32 @@
+class Currency
+
+    def initialize(amount, currency)
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+us_dollar = Currency.new(5,dollar)
+yen = Bottle.new("REI", 320)
+euro = Bottle.new("REI", 320)
